d20b1fac90a8ff2d7fc2b4fba4b9a8dd9fa61704
andreasnarep <andreasnarep2@gmail.com>
2024-05-01 09:50:34 +0300

    General commit.

--- src/main/java/dev/varo/inventory/InventoryApplication.java
+++ src/main/java/dev/varo/inventory/InventoryApplication.java
@@ -1,7 +1,9 @@
 package dev.varo.inventory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
